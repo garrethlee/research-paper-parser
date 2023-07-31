b836f4185cc1cc028ac22bf9e6105dec0ea9ff0c
Garreth Lee <garrethlee20@gmail.com>
2023-07-31 14:42:20 -0700

    refactored if statements in app.py to dictionary

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import streamlit as st
 from loguru import logger
 
 from config import *
-from journals import annurev, aom, asq, orgsci
 
 logger.add(sys.stdout, backtrace=True, diagnose=True)
 
@@ -38,10 +37,10 @@ else:
             "Upload the paper's PDF", on_change=set_converted_state, args=[False]
         )
     with col2:
+        # ADD PADDING
         st.text("")
         st.text("")
         st.text("")
-        # PADDING
         convert_button = st.button(
             "Convert",
             disabled=pdf_file == None,
@@ -54,16 +53,8 @@ else:
     if st.session_state["convert_clicked"] and pdf_file:
         pdf_file_contents = pdf_file.getvalue()
         doc = fitz.open("pdf", pdf_file_contents)
-
         try:
-            if journal == ORGSCI:
-                sections_df, references_df = orgsci.convert_pdf_to_dataframes(doc)
-            if journal == ANNUREV_ORGPSYCH:
-                sections_df, references_df = annurev.convert_pdf_to_dataframes(doc)
-            if journal == AOM:
-                sections_df, references_df = aom.convert_pdf_to_dataframes(doc)
-            if journal == ASQ:
-                sections_df, references_df = asq.convert_pdf_to_dataframes(doc)
+            sections_df, references_df = journal_map[journal](doc)
             st.session_state.convert_success = True
         except Exception as e:
             newline = "\n\n"
@@ -78,16 +69,16 @@ else:
         if st.session_state.convert_success:
             st.header("Sections")
             sections_display = st.dataframe(sections_df)
-            sections_download = st.download_button(
+            sections_download_button = st.download_button(
                 label="Download",
                 data=orgsci.sanitize_dataframe_for_download(sections_df).to_csv(),
                 file_name=f"{doc.name}_sections.csv",
             )
             st.header("References")
             references_display = st.dataframe(references_df)
-            references_download = st.download_button(
+            references_download_button = st.download_button(
                 label="Download",
-                data=annurev.sanitize_dataframe_for_download(references_df).to_csv(),
+                data=orgsci.sanitize_dataframe_for_download(references_df).to_csv(),
                 file_name=f"{doc.name}_references.csv",
             )
 
--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+from journals import annurev, aom, asq, orgsci
+
+
 JOURNALS = [
     "",
     "OrgSci",
@@ -7,3 +10,10 @@ JOURNALS = [
 ]
 
 DEFAULT_OPTION, ORGSCI, ANNUREV_ORGPSYCH, AOM, ASQ = JOURNALS
+
+journal_map = {
+    ORGSCI: orgsci.convert_pdf_to_dataframes,
+    ANNUREV_ORGPSYCH: annurev.convert_pdf_to_dataframes,
+    AOM: aom.convert_pdf_to_dataframes,
+    ASQ: asq.convert_pdf_to_dataframes,
+}
