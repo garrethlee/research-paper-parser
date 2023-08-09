8146ddc0104a93241ef0717e3ce49237d97cf0c1
Garreth Lee <garrethlee20@gmail.com>
2023-08-09 15:45:02 -0700

    simplified error logging to use wrapper

--- journals/annurev.py
+++ journals/annurev.py
@@ -1,13 +1,13 @@
 import re
-import traceback
-
 import fitz
 import pandas as pd
-from loguru import logger
 
+
+from log import log_traceback
 from section import *
 
 
+@log_traceback
 def get_sections(doc):
     """
     Generates the sections for the given document.
@@ -18,75 +18,67 @@ def get_sections(doc):
     Returns:
         list: The final sections of the document.
     """
-    try:
-        main_section = Section("", 100)
-        prev_size = 100
-        curr_section = main_section
 
-        for page in doc:
-            if page.number not in (doc[-1].number, doc[-2].number):
-                rect = fitz.Rect(
-                    page.rect.x0 + 20,
-                    page.rect.y0 + 20,
-                    page.rect.x1 - 20,
-                    page.rect.y1 - 30,
-                )
+    main_section = Section("", 100)
+    prev_size = 100
+    curr_section = main_section
 
-                blocks = page.get_text("dict", clip=rect)["blocks"]
-                for block in blocks:
-                    if "lines" in block.keys():
-                        spans = block["lines"]
-                        for span in spans:
-                            data = span["spans"]
-                            for lines in data:
-                                cur_size = round(lines["size"], 2)
+    for page in doc:
+        if page.number not in (doc[-1].number, doc[-2].number):
+            rect = fitz.Rect(
+                page.rect.x0 + 20,
+                page.rect.y0 + 20,
+                page.rect.x1 - 20,
+                page.rect.y1 - 30,
+            )
 
-                                if lines["text"].strip() in (
-                                    "Abstract",
-                                    "Keywords",
-                                    "LITERATURE CITED",
-                                ):
-                                    cur = Section(lines["text"], cur_size)
-                                    curr_section = main_section.children[-1].children[
-                                        -1
-                                    ]
-                                    curr_section.add_child(cur)
-                                    cur.set_parent(curr_section)
-                                    curr_section = cur
+            blocks = page.get_text("dict", clip=rect)["blocks"]
+            for block in blocks:
+                if "lines" in block.keys():
+                    spans = block["lines"]
+                    for span in spans:
+                        data = span["spans"]
+                        for lines in data:
+                            cur_size = round(lines["size"], 2)
 
-                                    prev_size = round(lines["size"], 2)
+                            if lines["text"].strip() in (
+                                "Abstract",
+                                "Keywords",
+                                "LITERATURE CITED",
+                            ):
+                                cur = Section(lines["text"], cur_size)
+                                curr_section = main_section.children[-1].children[-1]
+                                curr_section.add_child(cur)
+                                cur.set_parent(curr_section)
+                                curr_section = cur
 
-                                elif cur_size > prev_size:
-                                    curr_section = curr_section.backtrack_add(
-                                        lines["text"], cur_size
-                                    )
-                                    prev_size = curr_section.size
+                                prev_size = round(lines["size"], 2)
 
-                                elif cur_size == prev_size:
-                                    curr_section.extend(lines["text"])
+                            elif cur_size > prev_size:
+                                curr_section = curr_section.backtrack_add(
+                                    lines["text"], cur_size
+                                )
+                                prev_size = curr_section.size
 
-                                else:
-                                    cur = Section(lines["text"], cur_size)
-                                    curr_section.add_child(cur)
-                                    cur.set_parent(curr_section)
-                                    curr_section = cur
-                                    prev_size = round(lines["size"], 2)
+                            elif cur_size == prev_size:
+                                curr_section.extend(lines["text"])
 
-        final_sections = main_section.children[-1].children
+                            else:
+                                cur = Section(lines["text"], cur_size)
+                                curr_section.add_child(cur)
+                                cur.set_parent(curr_section)
+                                curr_section = cur
+                                prev_size = round(lines["size"], 2)
 
-        if len(final_sections) > 1:
-            return final_sections[-2].children + final_sections[-1].children
-        else:
-            return final_sections[-1].children
-    except:
-        logger.error(
-            f"Error occurred in 'structure_doc_by_size_and_font': {traceback.format_exc()}"
-        )
-        raise Exception(
-            f"Error occurred in 'structure_doc_by_size_and_font': {traceback.format_exc()}"
-        )
+    final_sections = main_section.children[-1].children
+
+    if len(final_sections) > 1:
+        return final_sections[-2].children + final_sections[-1].children
+    else:
+        return final_sections[-1].children
 
 
+@log_traceback
 def preprocess_sections(sections):
     """
     Preprocess sections.
@@ -97,23 +89,14 @@ def preprocess_sections(sections):
     Returns:
         list: Preprocessed sections.
     """
-    try:
-        # Preprocess sections
-        first_section = Section("Keywords", 0)
-        first_index = sections.index(first_section)
-        sections = sections[first_index:]
+    first_section = Section("Keywords", 0)
+    first_index = sections.index(first_section)
+    sections = sections[first_index:]
 
-        return sections
-    except:
-        logger.error(
-            f"Error occurred in 'preprocess_sections': {traceback.format_exc()}"
-        )
-        raise Exception(
-            f"Error occurred in 'preprocess_sections': {traceback.format_exc()}"
-        )
+    return sections
 
 
-### SAME
+@log_traceback
 def make_sections_dataframe(doc):
     """
     Generate a DataFrame containing the sections of a document.
@@ -124,33 +107,23 @@ def make_sections_dataframe(doc):
     Returns:
         tuple: A tuple containing the sections list and the sections DataFrame.
     """
+    sections = get_sections(doc)
+    sections = preprocess_sections(sections)
 
-    try:
-        # Get sections
-        sections = get_sections(doc)
-        sections = preprocess_sections(sections)
+    content_nest = {}
 
-        content_nest = {}
+    for section in sections:
+        content_nest[section.content] = [section.print_contents()]
 
-        for section in sections:
-            content_nest[section.content] = [section.print_contents()]
-
-        sections_df = pd.DataFrame(content_nest, index=["text"]).T
-        sections_df.name = doc.name
-        return sections, sections_df
-
-    except:
-        logger.error(
-            f"Error occurred in 'make_sections_dataframe': {traceback.format_exc()}"
-        )
-        raise Exception(
-            f"Error occurred in 'make_sections_dataframe': {traceback.format_exc()}"
-        )
+    sections_df = pd.DataFrame(content_nest, index=["text"]).T
+    sections_df.name = doc.name
+    return sections, sections_df
 
 
+@log_traceback
 def make_references_dataframe(sections, sections_df):
     """
-    Generate a dataframe of references from the given sections and sections_df.
+    Generate a df of references from the given sections and sections_df.
 
     Args:
         sections (list): List of sections.
@@ -163,35 +136,28 @@ def make_references_dataframe(sections, sections_df):
     references_text = sections[sections.index("LITERATURE CITED")].print_contents()
     references_clean = text_preprocess_for_reference_matching(references_text)
 
-    try:
-        for location, text in zip(sections_df.index[:-1], sections_df.values[:-1]):
-            in_text_citations = get_in_text_citations(text.item())
-            cleaned_in_text_citations = clean_in_text_citations(in_text_citations)
-            author_year_pairs = list(
-                filter(
-                    lambda x: x != None,
-                    map(process_citations, cleaned_in_text_citations),
-                )
+    for location, text in zip(sections_df.index[:-1], sections_df.values[:-1]):
+        in_text_citations = get_in_text_citations(text.item())
+        cleaned_in_text_citations = clean_in_text_citations(in_text_citations)
+        author_year_pairs = list(
+            filter(
+                lambda x: x != None,
+                map(process_citations, cleaned_in_text_citations),
             )
-            references_dictionary = find_citation_matches(
-                author_year_pairs, references_clean, references_dictionary, location
-            )
-
-        references_df = pd.DataFrame(
-            {k: [",".join(v)] for k, v in references_dictionary.items()},
-            index=["section"],
-        ).T.reset_index(names="reference")
-        return references_df
-
-    except:
-        logger.error(
-            f"Error occurred in 'make_references_dataframe': {traceback.format_exc()}"
         )
-        raise Exception(
-            f"Error occurred in 'make_references_dataframe': {traceback.format_exc()}"
+        references_dictionary = find_citation_matches(
+            author_year_pairs, references_clean, references_dictionary, location
         )
 
+    references_df = pd.DataFrame(
+        {k: [",".join(v)] for k, v in references_dictionary.items()},
+        index=["section"],
+    ).T.reset_index(names="reference")
 
+    return references_df
+
+
+@log_traceback
 def clean_in_text_citations(in_text_citations):
     """
     Cleans the in-text citations by removing unnecessary characters and splitting them into individual items.
@@ -202,24 +168,18 @@ def clean_in_text_citations(in_text_citations):
     Returns:
         list: A list of cleaned and split in-text citations.
     """
-    try:
-        return [
-            item
-            for group in [
-                re.sub(r"\(|\)|see also|’s", "", item).split(",")
-                for item in in_text_citations
-            ]
-            for item in group
+
+    return [
+        item
+        for group in [
+            re.sub(r"\(|\)|see also|’s", "", item).split(",")
+            for item in in_text_citations
         ]
-    except:
-        logger.error(
-            f"Error occurred in 'clean_in_text_citations': {traceback.format_exc()}"
-        )
-        raise Exception(
-            f"Error occurred in 'clean_in_text_citations': {traceback.format_exc()}"
-        )
+        for item in group
+    ]
 
 
+@log_traceback
 def text_preprocess_for_reference_matching(references_text):
     """
     Preprocesses the given references text for reference matching.
@@ -230,33 +190,25 @@ def text_preprocess_for_reference_matching(references_text):
     Returns:
         list: A list of cleaned references.
     """
-    try:
-        references_dirty = re.sub("\n", " ", references_text)
-        references = " ".join(references_dirty.split())
-        pattern = "[A-Z][A-Za-z, ]+[A-Z]{1,3}\. \d{4}\."
-        references_clean = re.findall(pattern, references)
+    references_dirty = re.sub("\n", " ", references_text)
+    references = " ".join(references_dirty.split())
+    pattern = "[A-Z][A-Za-z, ]+[A-Z]{1,3}\. \d{4}\."
+    references_clean = re.findall(pattern, references)
 
-        for idx, ref in enumerate(references_clean):
-            if idx == len(references_clean) - 1:
-                # All the way to the end
-                references_clean[idx] = references[references.find(ref) :]
-            else:
-                next_ref = references_clean[idx + 1]
-                references_clean[idx] = references[
-                    references.find(ref) : references.find(next_ref)
-                ]
+    for idx, ref in enumerate(references_clean):
+        if idx == len(references_clean) - 1:
+            # All the way to the end
+            references_clean[idx] = references[references.find(ref) :]
+        else:
+            next_ref = references_clean[idx + 1]
+            references_clean[idx] = references[
+                references.find(ref) : references.find(next_ref)
+            ]
 
-        return references_clean
-    except:
-        logger.error(
-            f"Error occurred in 'text_preprocess_for_reference_matching': {traceback.format_exc()}"
-        )
-        raise Exception(
-            f"Error occurred in 'text_preprocess_for_reference_matching': {traceback.format_exc()}"
-        )
+    return references_clean
 
 
-### DIFF
+@log_traceback
 def get_in_text_citations(text):
     """
     Extracts in-text citations from a given text.
@@ -291,37 +243,33 @@ def process_citations(citation: str):
             - If the citation contains 1 author, a list with the name and the year.
             - If the citation is invalid or does not match any of the cases, None is returned.
     """
-    try:
-        # case 1: 2 authors
-        if "&" in citation:
+
+    # case 1: 2 authors
+    if "&" in citation:
+        tokens = citation.split()
+        year = tokens[-1]
+        names = " ".join(tokens[:-1])
+        names_split = names.split("&")
+        return ((names_split[0].strip(), names_split[1].strip()), year.strip())
+
+    # case 2: et al
+    if "et al." in citation:
+        tokens = citation.split("et al.")
+        return ([tokens[0].strip()], tokens[1].strip())
+
+    # case 3: 1 author
+    else:
+        split = citation.split()
+        if len(split) == 1:
+            return None
+        else:
             tokens = citation.split()
             year = tokens[-1]
-            names = " ".join(tokens[:-1])
-            names_split = names.split("&")
-            return ((names_split[0].strip(), names_split[1].strip()), year.strip())
-
-        # case 2: et al
-        if "et al." in citation:
-            tokens = citation.split("et al.")
-            return ([tokens[0].strip()], tokens[1].strip())
-
-        # case 3: 1 author
-        else:
-            split = citation.split()
-            if len(split) == 1:
-                return None
-            else:
-                tokens = citation.split()
-                year = tokens[-1]
-                author = " ".join(tokens[:-1])
-                return ([author.strip()], year.strip())
-    except:
-        logger.error(f"Error occurred in 'process_citations': {traceback.format_exc()}")
-        raise Exception(
-            f"Error occurred in 'process_citations': {traceback.format_exc()}"
-        )
+            author = " ".join(tokens[:-1])
+            return ([author.strip()], year.strip())
 
 
+@log_traceback
 def find_citation_matches(author_year_pairs, full_references, data, location):
     """
     Find citation matches in the given list of author-year pairs and full references.
@@ -335,33 +283,26 @@ def find_citation_matches(author_year_pairs, full_references, data, location):
     Returns:
     - data (dict): The updated dictionary with the citation matches.
     """
-    try:
-        for author_year_pair in author_year_pairs:
-            authors, year = author_year_pair
-            for reference in full_references:
-                match = True
-                if year in reference:
-                    for author in authors:
-                        if author not in reference:
-                            match = False
-                    if match:
-                        dict_value = data.get(reference, [])
-                        if dict_value == []:
-                            data[reference] = []
-                        if location not in dict_value:
-                            data[reference] = data.get(reference, []) + [location]
-                else:
-                    continue
-        return data
-    except:
-        logger.error(
-            f"Error occurred in 'find_citation_matches': {traceback.format_exc()}"
-        )
-        raise Exception(
-            f"Error occurred in 'find_citation_matches': {traceback.format_exc()}"
-        )
+    for author_year_pair in author_year_pairs:
+        authors, year = author_year_pair
+        for reference in full_references:
+            match = True
+            if year in reference:
+                for author in authors:
+                    if author not in reference:
+                        match = False
+                if match:
+                    dict_value = data.get(reference, [])
+                    if dict_value == []:
+                        data[reference] = []
+                    if location not in dict_value:
+                        data[reference] = data.get(reference, []) + [location]
+            else:
+                continue
+    return data
 
 
+@log_traceback
 def convert_pdf_to_dataframes(doc):
     """
     Converts a PDF document into two separate dataframes.
@@ -370,24 +311,17 @@ def convert_pdf_to_dataframes(doc):
         doc (PDFDocument): The PDF document to be converted.
 
     Returns:
-        tuple: A tuple containing two dataframes. The first dataframe contains
-        the sections extracted from the document, while the second dataframe
+        tuple: A tuple containing two dataframes. The first df contains
+        the sections extracted from the document, while the second df
         contains the references extracted from the document.
     """
-    try:
-        sections, sections_df = make_sections_dataframe(doc)
-        references_df = make_references_dataframe(sections, sections_df)
-        return sections_df, references_df
-    except:
-        logger.error(
-            f"Error occurred in 'convert_pdf_to_dataframes': {traceback.format_exc()}"
-        )
-        raise Exception(
-            f"Error occurred in 'convert_pdf_to_dataframes': {traceback.format_exc()}"
-        )
+    sections, sections_df = make_sections_dataframe(doc)
+    references_df = make_references_dataframe(sections, sections_df)
+    return sections_df, references_df
 
 
-def sanitize_dataframe_for_download(dataframe):
+@log_traceback
+def sanitize_dataframe_for_download(df):
     """
     Sanitizes a pandas DataFrame for download by replacing newline characters and
     double quotes in string columns.
@@ -398,20 +332,10 @@ def sanitize_dataframe_for_download(dataframe):
     Returns:
         pandas.DataFrame: The sanitized DataFrame.
     """
-    try:
-        for col in dataframe.columns:
-            if dataframe[col].dtype == "object":
-                try:
-                    dataframe[col] = (
-                        dataframe[col].str.replace("\n", " ").str.replace('"', "'")
-                    )
-                except AttributeError:
-                    continue
-        return dataframe
-    except:
-        logger.error(
-            f"Error occurred in 'sanitize_dataframe_for_download': {traceback.format_exc()}"
-        )
-        raise Exception(
-            f"Error occurred in 'sanitize_dataframe_for_download': {traceback.format_exc()}"
-        )
+    for col in df.columns:
+        if df[col].dtype == "object":
+            try:
+                df[col] = df[col].str.replace("\n", " ").str.replace('"', "'")
+            except AttributeError:
+                continue
+    return df
